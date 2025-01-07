df8ea904a238348c33a139abbc2fb80af4143dc7
eduardokloppel <142949169+eduardokloppel@users.noreply.github.com>
2025-01-07 17:43:13 -0300

    update foto blog post

--- schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-// schemaTypes/index.ts
-
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-
-export const schemaTypes = [post, author, category, blockContent]
--- schemas/postType.ts
+++ schemas/postType.ts
@@ -1,7 +1,5 @@
-// schemas/postType.ts
-
-import {DocumentTextIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { DocumentTextIcon } from '@sanity/icons';
+import { defineArrayMember, defineField, defineType } from 'sanity';
 
 export const postType = defineType({
   name: 'post',
@@ -12,41 +10,49 @@ export const postType = defineType({
     defineField({
       name: 'title',
       type: 'string',
+      title: 'Title', // Adicione um título para o campo no Studio
     }),
     defineField({
       name: 'slug',
       type: 'slug',
+      title: 'Slug',
       options: {
         source: 'title',
+        maxLength: 96,
       },
     }),
     defineField({
       name: 'author',
       type: 'reference',
-      to: {type: 'author'},
+      title: 'Author',
+      to: { type: 'author' },
     }),
     defineField({
-      name: 'mainImage',
+      name: 'mainImage', // Campo para imagem principal
       type: 'image',
+      title: 'Main Image',
       options: {
-        hotspot: true,
+        hotspot: true, // Habilita recorte personalizado
       },
       fields: [
-        {
+        defineField({
           name: 'alt',
           type: 'string',
-          title: 'Alternative text',
-        }
-      ]
+          title: 'Alternative Text',
+          description: 'Texto descritivo para SEO e acessibilidade.',
+        }),
+      ],
     }),
     defineField({
       name: 'categories',
       type: 'array',
-      of: [defineArrayMember({type: 'reference', to: {type: 'category'}})],
+      title: 'Categories',
+      of: [defineArrayMember({ type: 'reference', to: { type: 'category' } })],
     }),
     defineField({
       name: 'publishedAt',
       type: 'datetime',
+      title: 'Published At',
     }),
     defineField({
       name: 'body',
@@ -58,11 +64,11 @@ export const postType = defineType({
     select: {
       title: 'title',
       author: 'author.name',
-      media: 'mainImage',
+      media: 'mainImage', // Exibe a imagem no preview
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
     },
   },
-})
+});
